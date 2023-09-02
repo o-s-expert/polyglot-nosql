d321fd93475d9a8b06121eb67b0c9e7e86995bb0
Otavio Santana <otaviopolianasantana@gmail.com>
2023-09-02 16:47:37 +0100

    chore: create 04-column project
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ 04-column/src/main/java/expert/os/labs/persistence/nosql/Person.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *  The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *  and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
+
+package expert.os.labs.persistence.nosql;
+
+
+import jakarta.nosql.Column;
+import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
+
+
+@Entity("Person")
+public class Person {
+
+    @Id("id")
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private Map<String, String> contacts;
+
+    public Person() {
+    }
+
+    Person(long id, String name, Map<String, String> contacts) {
+        this.id = id;
+        this.name = name;
+        this.contacts = contacts;
+    }
+
+    public long id() {
+        return id;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public Map<String, String> contacts() {
+        return Collections.unmodifiableMap(contacts);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", contacts=" + contacts +
+                '}';
+    }
+
+    public static PersonBuilder builder() {
+        return new PersonBuilder();
+    }
+}
